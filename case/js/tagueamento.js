9b3e1c7dee76979789cc86250e3e7b51fa87ab04
Breno Comin <breno_rc@hotmail.com>
2021-12-04 13:02:49 -0300

    Initial comiit
    First upload just to create webpage

---
+++ case/js/tagueamento.js
@@ -0,0 +1,14 @@
+// Preencha este arquivo com qualquer código que você necessite para realizar a
+// coleta, desde a biblioteca analytics.js, gtag.js ou o snippet do Google Tag 
+// Manager. No último caso, não é necessário implementar a tag <noscript>.
+// O ambiente dispõe da jQuery 3.5.1, então caso deseje, poderá utilizá-la
+// para fazer a sua coleta.
+// Caso tenha alguma dúvida sobre o case, não hesite em entrar em contato.
+
+
+ga('create', 'UA-12345-6', 'auto');
+
+ga(function(tracker) {
+  // Logs the tracker created above to the console.
+  console.log(tracker);
+});
