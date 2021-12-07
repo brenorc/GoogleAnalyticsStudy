f20cf97e048b0cb6c8cdc316ab9d6144203792c1
Breno Comin <breno_rc@hotmail.com>
2021-12-06 23:21:45 -0300

    Atualização v1
    Praticamente 100% funcional 
    - Todas as capturas realizadas conforme solicitado
    - Problemas com preenchimento muito rápido e auto-fill
    - Problemas com repetição de clicks no Lorem Ipsum

--- js/tagueamento.js
+++ js/tagueamento.js
@@ -4,11 +4,35 @@
 // O ambiente dispõe da jQuery 3.5.1, então caso deseje, poderá utilizá-la
 // para fazer a sua coleta.
 // Caso tenha alguma dúvida sobre o case, não hesite em entrar em contato.
+/*
+let timer,
+		timeoutVal = 1000; // tempo sem ações para enviar o evento em ms
+
+const status = document.getElementById('status');
+const typer = document.getElementById('typer');
+
+typer.addEventListener('keypress', handleKeyPress);
+typer.addEventListener('keyup', handleKeyUp);
+
+// when user is pressing down on keys, clear the timeout
+function handleKeyPress(e) {
+	window.clearTimeout(timer);
+  status.innerHTML = 'Typing...';
+}
+
+// when the user has stopped pressing on keys, set the timeout
+// if the user presses on keys before the timeout is reached, then this timeout is canceled
+function handleKeyUp(e) {
+	window.clearTimeout(timer); // prevent errant multiple timeouts from being generated
+	timer = window.setTimeout(() => {
+        ga('send', 'pageview');
+  }, timeoutVal);
+}
 
 
-ga('create', 'UA-12345-6', 'auto');
+var input =  document.getElementById('my-input');
 
-ga(function(tracker) {
-  // Logs the tracker created above to the console.
-  console.log(tracker);
-});
+// Add an event listener to monitor changes
+input.addEventListener('keyup', function (e) {
+    console.log('Value:', input.value);
+});*/
